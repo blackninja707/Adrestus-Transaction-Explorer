3f4a7bbd9f0699bd3a38dcae6bf0b9205820d084
cannonball008 <nicothomson659@gmail.com>
2023-12-04 03:10:50 -0500

    update footer

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -1,5 +1,11 @@
 const MainLayout = () => {
-  return <></>;
+  return (
+    <div className="w-full bg-board h-100vh">
+      <div className="max-w-[1408px] min-w-[343px] w-full flex flex-row justify-between items-center px-5 mx-auto">
+        <h2>Hello</h2>
+      </div>
+    </div>
+  );
 };
 
 export default MainLayout;
