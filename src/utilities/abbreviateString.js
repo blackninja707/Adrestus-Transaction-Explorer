bb3caff516a28c0c882b2a78d4ff7f2e73d7a80c
cannonball008 <nicothomson659@gmail.com>
2023-12-07 04:47:03 -0500

    update abbreviateString'

--- src/utilities/abbreviateString.js
+++ src/utilities/abbreviateString.js
@@ -1,4 +1,7 @@
 export function abbreviateString(input) {
+  if (input.length <= 8) {
+    return input; // Return the original string if it has 8 or fewer characters
+  }
   const f_letters = input.slice(0, 4);
   const l_letters = input.slice(-4);
 
