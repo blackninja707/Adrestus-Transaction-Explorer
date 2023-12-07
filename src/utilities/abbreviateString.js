3709882373fad997ebef651abcea732066c34d63
cannonball008 <nicothomson659@gmail.com>
2023-12-07 16:20:43 -0500

    add search function

--- src/utilities/abbreviateString.js
+++ src/utilities/abbreviateString.js
@@ -1,11 +1,6 @@
 export function abbreviateString(input) {
-<<<<<<< HEAD
   if (input.length <= 8) {
     return input; // Return the original string if it has 8 or fewer characters
-=======
-  if(input.length <= 8) {
-    return input;
->>>>>>> 6f1efbc41a79e7bc93590ca07286664e86981939
   }
   const f_letters = input.slice(0, 4);
   const l_letters = input.slice(-4);
